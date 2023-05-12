e378b1df5035800fd47a0c42555fd37b475f29de
Elias Braun <braunelias@tghd.email>
2023-05-12 18:24:26 +0200

    Added Secrurity

--- upload/deploy.sh
+++ upload/deploy.sh
@@ -1,4 +1,4 @@
 zip deploy ../TeamFaabs2023.pdf index.html main.css 247.jpg 
 
-curl -X POST -F "file=@deploy.zip" https://docat.tghd.eu/api/TeamFaabs-2023/1.0.0 
+curl -X POST -F "file=@deploy.zip" https://docat.tghd.eu/api/TeamFaabs-2023/1.0.0 -H "docat-api-key: c4a7af2099e801ba5324233824a4fa57"
     
